33af2780a7c03d79830dcb7ce2a7d5c121f956bf
Ilja Everilä <ilja.everila@siili.com>
2017-03-22 14:10:22 +0200

    Translate a small set of common text attributes.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import jsx from "babel-plugin-syntax-jsx";
 
-const DEFAULT_GETTEXT = 'gettext';
+const I18N_MSG_ATTRIBUTE = "i18nMsg";
+const DEFAULT_GETTEXT = "gettext";
+const TRANSLATABLE_ATTRIBUTES = [
+  "alt",
+  "placeholder",
+  "title"
+];
 
 export default function ({ types: t }) {
   function isTranslatableText(node) {
     return t.isJSXText(node) && node.value.trim();
+  }
+
+  function isTranslatableAttribute(attr) {
+    return TRANSLATABLE_ATTRIBUTES.includes(attr.name.name) &&
+      t.isStringLiteral(attr.value) &&
+      attr.value.value.trim();
   }
 
   function add(x, y, ...rest) {
@@ -34,37 +46,104 @@ export default function ({ types: t }) {
     );
   }
 
+  function hasTranslatableText(node) {
+    for (const child of node.children) {
+      if (isTranslatableText(child)) {
+        return true;
+      }
+    }
+
+    for (const attr of node.openingElement.attributes) {
+      if (isTranslatableAttribute(attr)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function translateAttributes(attributes) {
+    return attributes.map(attr => {
+      if (isTranslatableAttribute(attr)) {
+        return t.jSXAttribute(
+          attr.name,
+          t.jSXExpressionContainer(translatedText(attr.value.value))
+        );
+      } else {
+        return attr;
+      }
+    });
+  }
+
+  function simpleTranslation(path, stats) {
+    const { node } = path;
+
+    if (!hasTranslatableText(node)) {
+      return;
+    }
+
+    const newOpeningElement = t.jSXOpeningElement(
+      node.openingElement.name,
+      translateAttributes(node.openingElement.attributes),
+      node.openingElement.selfClosing
+    );
+
+    const newChildren = node.children.map(child => {
+      if (isTranslatableText(child)) {
+        return t.jSXExpressionContainer(translatedText(child.value));
+      } else {
+        return child;
+      }
+    });
+
+    return path.replaceWith(
+      t.jSXElement(
+        newOpeningElement,
+        node.closingElement,
+        newChildren,
+        node.selfClosing
+      )
+    );
+  }
+
+  function hasI18nMsg(node) {
+    for (const attr of node.openingElement.attributes) {
+      if (attr.name.name === I18N_MSG_ATTRIBUTE) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function complexTranslation(path, stats) {
+    console.warn("Complex translations note yet supported, ignoring i18nMsg attribute.");
+    const { node } = path;
+
+    const newOpeningElement = t.jSXOpeningElement(
+      node.openingElement.name,
+      node.openingElement.attributes.filter(
+        attr => attr.name.name !== I18N_MSG_ATTRIBUTE
+      ),
+      node.openingElement.selfClosing
+    );
+
+    return path.replaceWith(
+      t.jSXElement(
+        newOpeningElement,
+        node.closingElement,
+        node.children,
+        node.selfClosing
+      )
+    );
+  }
+
   const visitor = {
     JSXElement(path, stats) {
-      let hasTranslatableText = false;
-
-      for (const child of path.node.children) {
-        if (isTranslatableText(child)) {
-          hasTranslatableText = true;
-          break;
-        }
+      if (hasI18nMsg(path.node)) {
+        return complexTranslation(path, stats);
+      } else {
+        return simpleTranslation(path, stats);
       }
-
-      if (!hasTranslatableText) {
-        return;
-      }
-
-      const newChildren = path.node.children.map(child => {
-        if (isTranslatableText(child)) {
-          return t.jSXExpressionContainer(translatedText(child.value));
-        } else {
-          return child;
-        }
-      });
-
-      path.replaceWith(
-        t.jSXElement(
-          path.node.openingElement,
-          path.node.closingElement,
-          newChildren,
-          path.node.selfClosing
-        )
-      );
     }
   };
 
